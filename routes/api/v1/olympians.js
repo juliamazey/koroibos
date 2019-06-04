5d6ddd0c96c632a7c716d9939441c03e75c4cc3b
Julia Marco <marcootj@gmail.com>
2019-06-04 15:22:32 -0600

    Add conditional statements for query params

--- routes/api/v1/olympians.js
+++ routes/api/v1/olympians.js
@@ -6,18 +6,45 @@ pry = require('pryjs')
 
 // GET all olympians
 router.get("/", function(req, res) {
-  Olympian.findAll({
-    attributes: ['sport', 'team', 'age', 'name', [sequelize.fn('COUNT', sequelize.col('medal')), 'total_medals_won']],
-    group: ['Olympian.sport', 'Olympian.team', 'Olympian.age', 'Olympian.name']
-  })
-    .then(olympians => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(200).send(JSON.stringify({ olympians: olympians }));
-    })
-    .catch(error => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(400).send({ error })
-    });
+  if (req.query.age === 'oldest')
+   { Olympian.findOne({
+     attributes: ['name', 'team', 'age', 'sport', [sequelize.fn('COUNT', sequelize.col('medal')), 'total_medals_won']],
+     group: ['Olympian.sport', 'Olympian.team', 'Olympian.age', 'Olympian.name'],
+     order: [['age', 'DESC']]
+   })
+   .then(olympian => {
+     res.setHeader("Content-Type", "application/json");
+     res.status(200).send({olympian: olympian});
+   })
+   .catch(error => {
+     res.status(500).send({ error })
+   }); }
+   else if (req.query.age === 'youngest')
+     { Olympian.findOne({
+       attributes: ['name', 'team', 'age', 'sport', [sequelize.fn('COUNT', sequelize.col('medal')), 'total_medals_won']],
+       group: ['Olympian.sport', 'Olympian.team', 'Olympian.age', 'Olympian.name'],
+       order: [['age', 'ASC']]
+     })
+     .then(olympian => {
+       res.status(200).send({olympian: olympian});
+     })
+     .catch(error => {
+       res.status(500).send({ error })
+     }); }
+   else {
+     Olympian.findAll({
+       attributes: ['name', 'team', 'age', 'sport', [sequelize.fn('COUNT', sequelize.col('medal')), 'total_medals_won']],
+       group: ['Olympian.sport', 'Olympian.team', 'Olympian.age', 'Olympian.name']
+     })
+     .then(olympians => {
+       res.setHeader("Content-Type", "application/json");
+       res.status(200).send(JSON.stringify({ olympians: olympians }));
+     })
+     .catch(error => {
+       res.setHeader("Content-Type", "application/json");
+       res.status(500).send({ error })
+     });
+   }
 });
 
 module.exports = router;
